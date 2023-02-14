a5d5ea068cd2c60aa6627521b05752f1290e6e49
Mhabbat <119960455+Mhabbat@users.noreply.github.com>
2023-02-14 15:34:45 +0300

    Программа решающую задачу.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+{
+    if(a < b +c)
+    Console.WriteLine("Треугольник существует ");
+    else
+        if(b < a + c)
+    Console.WriteLine("Треугольник существует ");
+    
+    else if(c < a + b)
+    Console.WriteLine("Треугольник существует ");
+    else
+    Console.WriteLine("Треугольник не существует ");
+}
+Console.WriteLine(" ");
